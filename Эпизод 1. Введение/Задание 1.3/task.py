d6b16161aafded1a1d403bbaf203ccc4a24bde52
ilya <petrovskiji.143@gmail.com>
2023-02-28 15:44:35 +0600

    fix

--- Эпизод 1. Введение/Задание 1.3/task.py
+++ Эпизод 1. Введение/Задание 1.3/task.py
@@ -1,45 +1,57 @@
 def task_1(n):
     result = 0
-    for i in range(1, 11):
-        result += 1 / i
+    i=1
+    while i<11:
+        result+=1/i
+        i=i+1
     return result
 
 
 # Пример 2
 def task_2(x, n):
-    result = x
-    for i in range(1, 9):
-        result += x / (2 * i + 1)
+    result = 0
+    i = 1
+    while i < 18:
+        result += x / i
+        i = i + 2
     return result
 
 
 # Пример 3
 def task_3(n):
     result = 1
-    for i in range(2, n + 1):
+    i = 1
+    while i < n+1:
         result *= i
+        i = i + 1
     return result
 
 
 # Пример 4
 def task_4(x, n):
     result = 1
-    for i in range(2, 10):
-        result *= (x + i) / i
+    i = 2
+    while i < n+1:
+        result *= (x+i)/i
+        i = i + 1
     return result
 
 
 # Пример 5
 def task_5(x, n):
     result = 0
-    for i in range(1, n + 1):
-        result += x / 2
+    i=1
+    while i<n+1:
+        result+=x/2
+        i=i+1
     return result
 
 
 # Пример 6
 def task_6(n):
     result = 1
-    for i in range(1, 11):
-        result *= 2 * i
+    i = 2
+    while i < 21:
+        result *= i
+        i = i + 2
     return result
